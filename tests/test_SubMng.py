dc15d14a9ee8e2537ab628e0f498d6bd5ba1a8c2
Adeom <31165348+TomasMadeja@users.noreply.github.com>
2019-08-08 12:08:59 +0200

    Create test_SubMng.py

---
+++ tests/test_SubMng.py
@@ -0,0 +1,24 @@
+import unittest
+
+from TMLib import SubMng as Sub
+from TMLib import Definitions as TMdef
+
+class TMReWrapper(unittest.TestCase):
+    
+    def test_add_transformation_protocol(self):
+        Sub.subsribed_functions = {}
+        entry = {
+            Sub.PROCESSING : (lambda x, y : 0)
+            , Sub.PREPROCESSING : (lambda x, y : 1)
+            , Sub.VALIDATION : (lambda x, y : 2)
+        }
+
+        Sub.subscribe_protocol_transformation({'test' : entry})
+
+        self.assertTrue(len(Sub.subsribed_functions.keys()) == 1)
+        self.assertTrue(Sub.subsribed_functions.values()[0] is entry)
+        self.assertTrue(entry[Sub.PROCESSING](None, None) == 0 )
+        self.assertTrue(entry[Sub.PREPROCESSING](None, None) == 1 )
+        self.assertTrue(entry[Sub.VALIDATION](None, None) == 2 )
+
+
